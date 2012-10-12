39086171f877a444726875361613e8baa8a00861
Bob Edmonston <bob@inzan.com>
2012-10-11 21:18:13 -0700

    Cleanup to PageViewController

--- Unbound5/Unbound5/PageViewController.m
+++ Unbound5/Unbound5/PageViewController.m
@@ -98,9 +98,6 @@ static NSString *ResolveName(NSString *aName)
 - (IBAction)goBack:sender;
 {
     [self.parentWindowController showMainView];
-    //[self.view setHidden:YES];
-    //[self.parentViewController unhideSubviews];
-    //[(MainWindowController *)self.view.window setContentView showMainView];
 }
 
 - (IBAction)editPhoto:(id)sender;
@@ -119,73 +116,13 @@ static NSString *ResolveName(NSString *aName)
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         [PageViewController load];
-        // Initialization code here.
-        //[self updateData];
-        //[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateData) name:@"UB_PATH_CHANGED" object:nil];
     }
     
     return self;
 }
 
-/*-(void) updateData
-{
-    NSMutableArray *tmp = [NSMutableArray arrayWithCapacity:350];
-    for (IKBBrowserItem *item in [[AppDelegate applicationDelegate]
-                                  imagesArray])
-    {
-        [tmp addObject:item.image];
-    }
-	//Allocate some space for the data source
-    self.pagerData = tmp;
-    if (!self.pagerData) {
-        self.pagerData = [[NSMutableArray alloc] initWithCapacity:10];
-    }
-	
-    [self.pageController setArrangedObjects:self.pagerData];
-    [self.view setNeedsDisplay:YES];
-}*/
 
 - (void)updateData {
-    //NSURL *dirURL = [NSURL fileURLWithPath:@"/Users/inzan/Dropbox/Camera Uploads"];//[[NSFileManager defaultManager] url];
-    //NSURL *dirURL = [[NSFileManager defaultManager] URLForDirectory:NSPicturesDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
-    /*NSURL *dirURL = [[NSBundle mainBundle] resourceURL];
-    if (self.album == nil) {
-        self.directoryURL = dirURL;
-    } else {
-        DLog(@"PageController called with no directory set");
-        assert(NO);
-    }
-    
-    // load all the necessary image files by enumerating through the bundle's Resources folder,
-    // this will only load images of type "kUTTypeImage"
-    //
-    self.pageController.delegate = self;
-    self.pagerData = [[NSMutableArray alloc] initWithCapacity:1];
-    
-    
-    
-    NSDirectoryEnumerator *itr = [[NSFileManager defaultManager] enumeratorAtURL:self.directoryURL includingPropertiesForKeys:[NSArray arrayWithObjects:NSURLLocalizedNameKey, NSURLEffectiveIconKey, NSURLIsDirectoryKey, NSURLTypeIdentifierKey, nil] options:NSDirectoryEnumerationSkipsHiddenFiles | NSDirectoryEnumerationSkipsPackageDescendants | NSDirectoryEnumerationSkipsSubdirectoryDescendants errorHandler:nil];
-    
-    // loop through tags and set them up in a background thread
-    / *dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void) {
-        NSMutableArray *tmpArray = [NSMutableArray arrayWithCapacity:10];
-        for (NSURL *url in itr) {
-            
-            if (YES )//| i<6000)
-            {
-                NSString *utiValue;
-                [url getResourceValue:&utiValue forKey:NSURLTypeIdentifierKey error:nil];
-                
-                if (UTTypeConformsTo((__bridge CFStringRef)(utiValue), kUTTypeImage)) {
-                    NSImage *image = [[NSImage alloc] initByReferencingURL:url];
-                    [tmpArray addObject:image];
-                    //i++;
-                }
-            } //else {
-               //[self.pagerData addObject:[NSImage imageNamed:@"earring.jpg"]];
-               //}
-        }
-    });*/
     
     self.pageController.delegate = self;
     self.pagerData = [[NSMutableArray alloc] initWithCapacity:self.album.photos.count];
@@ -222,32 +159,7 @@ static NSString *ResolveName(NSString *aName)
             [self.pagerData addObject:[NSImage imageNamed:@"NSImage"]];
         }
     }
-        /*NSURL *url = anItem.filePathURL;
-        NSString *utiValue;
-    
-        [url getResourceValue:&utiValue forKey:NSURLTypeIdentifierKey error:nil];
-        
-        if (UTTypeConformsTo((__bridge CFStringRef)(utiValue), kUTTypeImage)) {
-            //NSImage *image = [[NSImage alloc] initWithContentsOfURL:url];
-            NSImage *image = [[NSImage alloc] initByReferencingURL:url];
-            [self.pagerData addObject:image];
-        } else if (UTTypeConformsTo((__bridge CFStringRef)(utiValue), kUTTypeMovie)) {
-            //TODO figure out movie stuff
-            //NSImage *image = [[NSImage alloc] initWithContentsOfURL:url];
-            NSError *error=nil;
-            id movie = [QTMovie movieWithURL:url error:&error];
-            if (!movie)
-            {
-                NSLog(@"error loading movie : %@", error);
-                [self.pagerData addObject:[NSImage imageNamed:@"NSFolder"]];
-            } else {
-                [self.pagerData addObject:movie];
-            }
-        } else {
-            [self.pagerData addObject:[NSImage imageNamed:@"NSFolder"]];
-            NSLog(@"Undifientifed type : %@", utiValue); //@"public.folder"
-        }
-    }*/
+
     
     // set the first image in our list to the main magnifying view
     if ([self.pagerData count] > 0) {
@@ -255,22 +167,13 @@ static NSString *ResolveName(NSString *aName)
         NSInteger index = [self.album.photos indexOfObject:self.initialSelectedItem];
         [self.pageController setSelectedIndex:index];
     }
-    
-    //[self.view setNeedsDisplay:YES];
 }
 
 
 - (void)awakeFromNib
 {
     self.pageController.transitionStyle = NSPageControllerTransitionStyleHorizontalStrip;
-    
-    if (NO || [self.pagerData count] > 0) {
-        //[self.pagerData makeObjectsPerformSelector:@selector(thumbnailImage)];
-        [self.pageController setArrangedObjects:self.pagerData];
-    } else {
-    //[self.view setNeedsDisplay:YES];
-        [self updateData];
-    }
+    [self updateData];
 }
 
 @end
@@ -282,8 +185,7 @@ static NSString *ResolveName(NSString *aName)
         return @"picture";
     } else {
         return @"video";
-    }
-    //return [NSString stringWithFormat:@"picture-%@", [NSDate date]];
+    };
 }
 
 - (NSViewController *)pageController:(NSPageController *)pageController viewControllerForIdentifier:(NSString *)identifier {
@@ -307,29 +209,7 @@ static NSString *ResolveName(NSString *aName)
     if (!isRepreparingOriginalView) {
         [(NSScrollView*)viewController.view setMagnification:1.0];
     }*/
-    
-    // Since we implement this delegate method, we are reponsible for setting the representedObject.
-    /*if ([NSImage imageNamed:@"earring.jpg"] == object)
-    {
-        SearchItem *anItem = [self.searchData objectAtIndex:pageController.selectedIndex];
-        NSImage *anImage = [[NSImage alloc] initByReferencingFile:anItem.filePathURL.path];
-        
-        viewController.representedObject = anImage;
-    } else {
-        viewController.representedObject = object;
-    }
-    return;
-    
-    SearchItem *anItem = (SearchItem *)viewController.representedObject;
-    BOOL isItem = [viewController.representedObject respondsToSelector:@selector(thumbnailImage)];
-    //[anItem thumbnailImage];
-    //viewController.representedObject = [[NSImage alloc] initByReferencingFile:anItem.filePathURL.path];
-    if (!isItem || [anItem thumbnailImage] == nil) {
-        viewController.representedObject = [NSImage imageNamed:@"earring.jpg"];
-    } else {
-        viewController.representedObject = [NSImage imageNamed:@"image1.jpg"];
-        //viewController.representedObject = anItem.thumbnailImage;
-    }*/  
+
 }
 
 - (void)pageControllerWillStartLiveTransition:(NSPageController *)pageController {
@@ -337,34 +217,10 @@ static NSString *ResolveName(NSString *aName)
     //self.initialSelectedObject = [pageController.arrangedObjects objectAtIndex:pageController.selectedIndex];
 }
 
-- (void)pageControllerWillStartLiveTransition1:(NSPageController *)aPageController {
-    // Remember the initial selected object so we can determine when a cancel occurred.
-    //id anObject = [aPageController.arrangedObjects objectAtIndex:aPageController.selectedIndex];
-    
-    /*if ([NSImage imageNamed:@"earring.jpg"] == self.initialSelectedObject)
-    {
-        SearchItem *anItem = [self.searchData objectAtIndex:aPageController.selectedIndex];
-        NSImage *anImage = [[NSImage alloc] initByReferencingFile:anItem.filePathURL.path];
-        
-        anObject = anImage;
-    }*/     
-    //self.initialSelectedObject = anObject;
-    return;
-}
 
 - (void)pageController:(NSPageController *)pageController didTransitionToObject:(id)object
 {
     NSLog(@"didTransitionToObject : %@", object);
-    /*if ([NSImage imageNamed:@"earring.jpg"] == object)
-    {
-        SearchItem *anItem = [self.searchData objectAtIndex:pageController.selectedIndex];
-        NSImage *anImage = [[NSImage alloc] initByReferencingFile:anItem.filePathURL.path];
-        
-        self.pageController.selectedViewController.representedObject = anImage;
-    } else {
-        //viewController.representedObject = object;
-    }
-    return;*/
 }
 
 - (void)pageControllerDidEndLiveTransition:(NSPageController *)aPageController {
@@ -372,36 +228,5 @@ static NSString *ResolveName(NSString *aName)
 }
 
 
-///BELEOW IS THE OLD STUFF TRYING TO USE IKIMAGEVIEW
-
-/*- (NSString *)pageController:(NSPageController *)pageController identifierForObject:(id)object {
- //[(SearchItem *)object thumbnailImage];
- return [(SearchItem *)object imageUID];
- //return [NSString stringWithFormat:@"picture-%ld", pageController.selectedIndex];
- }
- 
- - (NSViewController *)pageController:(NSPageController *)pageController viewControllerForIdentifier:(NSString *)identifier {
- IKImageViewController *aView =  [[IKImageViewController alloc] initWithNibName:@"IKImageViewController" bundle:nil];
- aView.representedObject = [NSURL fileURLWithPath:identifier isDirectory:NO];
- return aView;
- }
- 
- 
- -(void)pageController:(NSPageController *)pageController prepareViewController:(NSViewController *)viewController withObject:(id)object {
- // viewControllers may be reused... make sure to reset important stuff like the current magnification factor.
- 
- // Normally, we want to reset the magnification value to 1 as the user swipes to other images. However if the user cancels the swipe, we want to leave the original magnificaiton and scroll position alone.
- 
- BOOL isRepreparingOriginalView = (self.initialSelectedObject && self.initialSelectedObject == object) ? YES : NO;
- if (!isRepreparingOriginalView) {
- //[(NSScrollView*)viewController.view setMagnification:1.0];
- //[[(IKImageViewController *)viewController imageView] setZoomFactor:1.0];
- }
- 
- //NSImage *image = [[NSImage alloc] initWithContentsOfURL:[object filePathURL]];
- // Since we implement this delegate method, we are reponsible for setting the representedObject.
- viewController.representedObject = [object filePathURL];
- //viewController.view.frame = self.parentWindowController.window.contentView.bounds;
- }*/
 
 @end
