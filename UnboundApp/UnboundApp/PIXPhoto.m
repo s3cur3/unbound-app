f3e64a020919c2a7c36dcf3280c1f3e5675c70bc
Scott Sykora <scott.sykora@gmail.com>
2013-02-18 12:20:08 -0800

    fix so fullscreen view works again

--- UnboundApp/UnboundApp/PIXPhoto.m
+++ UnboundApp/UnboundApp/PIXPhoto.m
@@ -132,7 +132,7 @@ const CGFloat kThumbnailSize = 200.0f;
     [[NSNotificationQueue defaultQueue] enqueueNotification:aNotification postingStyle:NSPostASAP coalesceMask:NSNotificationCoalescingOnName forModes:nil];
     */
     
-    //If this is the coverPhoto of an album send a notification to the album to update it's thumb as well
+    //If this is the datePhoto of an album send a notification to the album to update it's thumb as well
     if (self.album.datePhoto == self) {
         //NSNotification *albumNotification = [NSNotification notificationWithName:AlbumDidChangeNotification object:self.album];
         //[[NSNotificationQueue defaultQueue] enqueueNotification:albumNotification postingStyle:NSPostASAP coalesceMask:NSNotificationCoalescingOnSender forModes:nil];
@@ -496,5 +496,80 @@ const CGFloat kThumbnailSize = 200.0f;
 //    return didSomething;
 //}
 
+
+@end
+
+
+@implementation PIXPhoto(IKImageBrowserItem)
+
+
+/*!
+ @method imageUID
+ @abstract Returns a unique string that identify this data source item (required).
+ @discussion The image browser uses this identifier to keep the correspondance between its cache and the data source item
+ */
+- (NSString *)  imageUID;  /* required */
+{
+    return self.path;
+}
+
+/*!
+ @method imageRepresentationType
+ @abstract Returns the representation of the image to display (required).
+ @discussion Keys for imageRepresentationType are defined below.
+ */
+- (NSString *) imageRepresentationType; /* required */
+{
+    return IKImageBrowserPathRepresentationType;
+}
+
+/*!
+ @method imageRepresentation
+ @abstract Returns the image to display (required). Can return nil if the item has no image to display.
+ */
+- (id) imageRepresentation; /* required */
+{
+    return self.path;
+}
+
+#pragma mark -
+#pragma mark Optional Methods IKImageBrowserItem Informal Protocol
+
+/*!
+ @method imageVersion
+ @abstract Returns a version of this item. The receiver can return a new version to let the image browser knows that it shouldn't use its cache for this item
+ */
+- (NSUInteger) imageVersion;
+{
+    return 1;
+}
+
+/*!
+ @method imageTitle
+ @abstract Returns the title to display as a NSString. Use setValue:forKey: with IKImageBrowserCellsTitleAttributesKey on the IKImageBrowserView instance to set text attributes.
+ */
+- (NSString *) imageTitle;
+{
+    return self.name;
+}
+
+/*!
+ @method imageSubtitle
+ @abstract Returns the subtitle to display as a NSString. Use setValue:forKey: with IKImageBrowserCellsSubtitleAttributesKey on the IKImageBrowserView instance to set text attributes.
+ */
+- (NSString *) imageSubtitle;
+{
+    return self.name;
+}
+
+/*!
+ @method isSelectable
+ @abstract Returns whether this item is selectable.
+ @discussion The receiver can implement this methods to forbid selection of this item by returning NO.
+ */
+- (BOOL) isSelectable;
+{
+    return YES;
+}
 
 @end
