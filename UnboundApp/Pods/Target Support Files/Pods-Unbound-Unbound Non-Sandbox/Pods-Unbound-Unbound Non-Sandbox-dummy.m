aeb502a192962fd26d35a418b6e590cf2f8802b6
Ryan Harter <ryanjharter@gmail.com>
2016-02-26 11:40:08 -0600

    Adds pod support and move Sparkle to the pod.

---
+++ UnboundApp/Pods/Target Support Files/Pods-Unbound-Unbound Non-Sandbox/Pods-Unbound-Unbound Non-Sandbox-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Unbound_Unbound_Non_Sandbox : NSObject
+@end
+@implementation PodsDummy_Pods_Unbound_Unbound_Non_Sandbox
+@end
---
+++ UnboundApp/Pods/Target Support Files/Pods-Unbound-Unbound Non-Sandbox/Pods-Unbound-Unbound Non-Sandbox-umbrella.h
@@ -0,0 +1,6 @@
+#import <Cocoa/Cocoa.h>
+
+
+FOUNDATION_EXPORT double Pods_Unbound_Unbound_Non_SandboxVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Unbound_Unbound_Non_SandboxVersionString[];
+
