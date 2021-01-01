c1583d1170ef0eeaffcb12d0ab903aa1018baf3d
Tyler Young <tyler@x-plane.com>
2020-12-31 20:47:37 -0600

    fix (library picker): Set a window title

--- UnboundApp/UnboundApp/Windows/LibraryPicker/LibraryPickerObjCBridge.swift
+++ UnboundApp/UnboundApp/Windows/LibraryPicker/LibraryPickerObjCBridge.swift
@@ -2,10 +2,11 @@ import Cocoa
 import SwiftUI
 
 class SwiftUIWindowController<RootView: View>: NSWindowController {
-    convenience init(rootView: RootView) {
+    convenience init(rootView: RootView, title: String) {
         let hostingController = NSHostingController(rootView: rootView.frame(width: 400, height: 300))
         let window = NSWindow(contentViewController: hostingController)
         window.setContentSize(NSSize(width: 400, height: 300))
+        window.title = title
         self.init(window: window)
     }
 }
@@ -13,6 +14,6 @@ class SwiftUIWindowController<RootView: View>: NSWindowController {
 @objc class LibraryPickerObjCBridge: NSView {
     @objc class func makeLibraryPicker() -> NSWindowController {
         // TODO: pull dirs from prefs
-        SwiftUIWindowController(rootView: LibraryPicker(dirs: []))
+        SwiftUIWindowController(rootView: LibraryPicker(dirs: []), title: "Select Main Photo Directories")
     }
 }
